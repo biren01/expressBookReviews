a9dbaa89c85d549291ac622c6f77829212e98a3b
biren <raibiren@gmail.com>
2026-04-03 16:16:35 -0400

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,6 +13,10 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  return res.status(200).json({
+    message: "List of Books", 
+    data: books
+});  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
